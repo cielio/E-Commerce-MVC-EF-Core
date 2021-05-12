cd8c9a3cb09b0faf50d51d9153184b4a395ccac1
cielio <cielion@hotmail.com>
2021-05-12 07:23:04 -0300

    Add Layout e Modificou a Rota Padrão do MVC

--- Startup.cs
+++ Startup.cs
@@ -39,11 +39,12 @@ namespace CasaDoCodigo
 
             app.UseStaticFiles();
 
+            //Define a pagina padrao
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Pedido}/{action=Carrossel}/{id?}");
             });
         }
     }
