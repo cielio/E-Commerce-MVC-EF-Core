b1e2e4b7dc39154dddd8b28d2cff66b5707d0f2e
cielio <cielion@hotmail.com>
2021-05-13 07:35:34 -0300

    Relacionamentos e Migrações

---
+++ ApplicationContext.cs
@@ -0,0 +1,36 @@
+﻿using CasaDoCodigo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+        }
+        // Fazer Mapeamento
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Produto>().HasKey(t => t.Id);
+
+            // Chave primaria
+            modelBuilder.Entity<Pedido>().HasKey(t => t.Id);
+            // Relacionamento de um para muito ("Pedido tem varios Itens")
+            modelBuilder.Entity<Pedido>().HasMany(t => t.Itens).WithOne(t => t.Pedido);
+            modelBuilder.Entity<Pedido>().HasOne(t => t.Cadastro).WithOne(t => t.Pedido).IsRequired();
+
+            modelBuilder.Entity<ItemPedido>().HasKey(t => t.Id);
+            modelBuilder.Entity<ItemPedido>().HasOne(t => t.Pedido);
+            modelBuilder.Entity<ItemPedido>().HasOne(t => t.Produto);
+
+            modelBuilder.Entity<Cadastro>().HasKey(t => t.Id);
+            modelBuilder.Entity<Cadastro>().HasOne(t => t.Pedido);
+        }
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CasaDoCodigo.Models
+{
+    public class Produto : BaseModel
+    {
+        [Required]
+        public string Codigo { get; private set; }
+        [Required]
+        public string Nome { get; private set; }
+        [Required]
+        public decimal Preco { get; private set; }
+
+        public Produto() { }
+
+        public Produto(string codigo, string nome, decimal preco)
+        {
+            this.Codigo = codigo;
+            this.Nome = nome;
+            this.Preco = preco;
+        }
+    }
+}
