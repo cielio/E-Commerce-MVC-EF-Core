e1a5bd340f61d0f3ba528a80d8d91795eb8db909
cielio <cielion@hotmail.com>
2021-05-13 22:30:20 -0300

    Lendo Arquivo Json de Produtos

--- Startup.cs
+++ Startup.cs
@@ -53,7 +53,7 @@ namespace CasaDoCodigo
                     template: "{controller=Pedido}/{action=Carrossel}/{id?}");
             });
 
-            serviceProvider.GetService<DataService>().InicializaDB();
+            serviceProvider.GetService<IDataService>().InicializaDB();
         }
     }
 }
